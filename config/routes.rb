aeb8492b20cee3e1999fd1ba5c0f9ea4608a4902
Dorothy Addie <daddie@smith.edu>
2022-03-22 15:09:40 -0400

    sets root to about page, deletes stations page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  #root to: "stations#index"
-  get "about", to: "about#index"
+  root to: "about#index"
   get "login", to: "login#index"
   get "membership", to: "membership#index"
   get "map", to: "map#index"
