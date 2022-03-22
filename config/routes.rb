73cc69dff5e08d23a88eb71dca4810ad2af9d570
Zephyr Wang <zephyrwang@Zephyrs-Mac.local>
2022-03-22 15:29:49 -0400

    change homepage to about

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   #root to: "stations#index"
  
   
-  get "about", to: "about#index"
+  
+  get "articles", to: "articles#index"
   get "login", to: "login#index"
   get "membership", to: "membership#index"
   get "map", to: "map#index"
   get "register", to: "register#index"
-  root 'map#index'
+  get 'stations/index'
+  root 'about#index'
 end
 
