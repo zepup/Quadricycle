d2aad5db29defd617e1f8a26f7b984a12db0c815
i-m-foster <irenefoster64@gmail.com>
2022-03-29 14:05:38 -0400

    moving map code to js file and map to map page

--- app/assets/javascript/leaflet.js
+++ app/assets/javascript/leaflet.js
@@ -1,14 +1,5 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([42.33, -72.64], 13);
 
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoiaWZvc3RlcjI1IiwiYSI6ImNsMWNkams1ZTA2bDkzZm40ejY3a2kzOWkifQ.pg8wyv6smCQ2EGWshD7IKA'
-// }).addTo(map);
-
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
