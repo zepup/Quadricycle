52d87c33f401bb8a2ed4a5a497e09657394b6cfd
Zephyr Wang <zephyrwang@Zephyrs-Mac.local>
2022-03-31 23:06:33 -0400

    sign-in navbar map shown

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
+
   #root to: "stations#index"
  
   
-  
+  get "about/about"
   get "articles", to: "articles#index"
-=======
+
   devise_for :users
   root to: "about#index"
->>>>>>> Stashed changes
+
   get "login", to: "login#index"
   get "membership", to: "membership#index"
   get "map", to: "map#index"
   get "register", to: "register#index"
   get 'stations/index'
-  root 'about#index'
 end
 
