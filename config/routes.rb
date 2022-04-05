a3a80bb13687fde6b0d70cb4a11be2dda7f5f537
Madeleine King <madeleineking@Madeleines-MacBook-Air-2.local>
2022-04-04 21:20:46 -0400

    fixing membership page links and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   root to: "about#index"
 
   get "login", to: "login#index"
-  get "membership", to: "membership#index"
+  get "membership", to: "memberships#index"
   get "map", to: "map#index"
   get "register", to: "register#index"
 end
